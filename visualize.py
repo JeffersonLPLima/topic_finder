12a8c16de7a31a7bccff4471a908297982b0f8de
JeffersonLPLima <jeffersonlima163@gmail.com>
2021-10-26 00:14:20 -0300

    add topic visualization

---
+++ visualize.py
@@ -0,0 +1,86 @@
+
+from matplotlib import pyplot as plt
+# Source https://www.machinelearningplus.com/nlp/topic-modeling-visualization-how-to-present-results-lda-models/
+from matplotlib.patches import Rectangle
+import matplotlib.colors as mcolors
+
+#FIXME
+def sentences_chart(lda_model, corpus, start = 0, end = 13):
+    corp = corpus[start:end]
+    mycolors = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+
+    fig, axes = plt.subplots(end-start, 1, figsize=(20, (end-start)*0.95), dpi=160)       
+    axes[0].axis('off')
+    for i, ax in enumerate(axes):
+        if i > 0:
+            corp_cur = corp[i-1] 
+            topic_percs, wordid_topics, wordid_phivalues = lda_model[corp_cur]
+            word_dominanttopic = [(lda_model.id2word[wd], topic[0]) for wd, topic in wordid_topics]    
+            ax.text(0.01, 0.5, "Doc " + str(i-1) + ": ", verticalalignment='center',
+                    fontsize=16, color='black', transform=ax.transAxes, fontweight=700)
+
+            # Draw Rectange
+            topic_percs_sorted = sorted(topic_percs, key=lambda x: (x[1]), reverse=True)
+            ax.add_patch(Rectangle((0.0, 0.05), 0.99, 0.90, fill=None, alpha=1, 
+                                   color=mycolors[topic_percs_sorted[0][0]], linewidth=2))
+
+            word_pos = 0.06
+            for j, (word, topics) in enumerate(word_dominanttopic):
+                if j < 14:
+                    ax.text(word_pos, 0.5, word,
+                            horizontalalignment='left',
+                            verticalalignment='center',
+                            fontsize=16, color=mycolors[topics],
+                            transform=ax.transAxes, fontweight=700)
+                    word_pos += .009 * len(word)  # to move the word for the next iter
+                    ax.axis('off')
+            ax.text(word_pos, 0.5, '. . .',
+                    horizontalalignment='left',
+                    verticalalignment='center',
+                    fontsize=16, color='black',
+                    transform=ax.transAxes)       
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.suptitle('Sentence Topic Coloring for Documents: ' + str(start) + ' to ' + str(end-2), fontsize=22, y=0.95, fontweight=700)
+    plt.tight_layout()
+    plt.show()
+
+
+def generate_html_vis(lda_model, bow_corpus):
+    import pyLDAvis.gensim_models
+    vis =  pyLDAvis.gensim_models.prepare(lda_model, bow_corpus, dictionary=lda_model.id2word)
+    print("generating topic visuzliation -> ./topics.html")
+    pyLDAvis.save_html(vis, 'topics.html')
+
+#FIXME
+def word_count_barplot(lda_model, raw_texts):
+    from collections import Counter
+    import pandas as pd
+    topics = lda_model.show_topics(formatted=False)
+    data_flat = [w for w_list in raw_texts for w in w_list]
+    counter = Counter(data_flat)
+
+    out = []
+    for i, topic in topics:
+        for word, weight in topic:
+            out.append([word, i , weight, counter[word]])
+
+    df = pd.DataFrame(out, columns=['word', 'topic_id', 'importance', 'word_count'])        
+
+    # Plot Word Count and Weights of Topic Keywords
+    fig, axes = plt.subplots(2, 2, figsize=(16,10), sharey=True, dpi=160)
+    cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+    for i, ax in enumerate(axes.flatten()):
+        ax.bar(x='word', height="word_count", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.5, alpha=0.3, label='Word Count')
+        ax_twin = ax.twinx()
+        ax_twin.bar(x='word', height="importance", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.2, label='Weights')
+        ax.set_ylabel('Word Count', color=cols[i])
+        ax_twin.set_ylim(0, 0.030); ax.set_ylim(0, 3500)
+        ax.set_title('Topic: ' + str(i), color=cols[i], fontsize=16)
+        ax.tick_params(axis='y', left=False)
+        ax.set_xticklabels(df.loc[df.topic_id==i, 'word'], rotation=30, horizontalalignment= 'right')
+        ax.legend(loc='upper left'); ax_twin.legend(loc='upper right')
+
+    fig.tight_layout(w_pad=2)    
+    fig.suptitle('Word Count and Importance of Topic Keywords', fontsize=22, y=1.05)    
+    plt.savefig('word_count.png')
